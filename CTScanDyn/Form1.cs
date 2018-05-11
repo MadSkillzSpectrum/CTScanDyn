fb7d0b04254d653a34469b834b31c831192f3e10
Alexander A <tusur.alexander@gmail.com>
2018-05-11 06:40:12 +0600

    Initial commit

---
+++ CTScanDyn/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Dicom;
+using Dicom.Imaging;
+using Dicom.Imaging.Mathematics;
+
+namespace CTScanDyn
+{
+    public partial class Form1 : Form
+    {
+        public Bitmap Original1;
+        public Bitmap Original2;
+
+        private List<ImageData> ImageSetBefore = new List<ImageData>();
+        private List<ImageData> ImageSetAfter = new List<ImageData>();
+        private List<ImageData> ImageSetResult = new List<ImageData>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            button2.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FillImageSet(ImageSetBefore, "before");
+            button2.Enabled = true;
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FillImageSet(ImageSetAfter, "after");
+            TrimExtra();
+        }
+
+        private void TrimExtra()
+        {
+            if (ImageSetBefore.Count > ImageSetAfter.Count)
+            {
+                ImageSetBefore.RemoveRange(ImageSetAfter.Count-1, ImageSetBefore.Count - ImageSetAfter.Count);
+            }
+            else if (ImageSetBefore.Count < ImageSetAfter.Count)
+            {
+                ImageSetAfter.RemoveRange(ImageSetBefore.Count - 1, ImageSetAfter.Count - ImageSetBefore.Count);
+            }
+        }
+
+        public List<Point> Rotate(List<Point> points, Point pivot, double angleDegree)
+        {
+            double angle = angleDegree * Math.PI / 180;
+            double cos = Math.Cos(angle);
+            double sin = Math.Sin(angle);
+            List<Point> rotated = new List<Point>();
+            foreach (Point point in points)
+            {
+                int dx = point.X - pivot.X;
+                int dy = point.Y - pivot.Y;
+                double x = cos * dx - sin * dy + pivot.X;
+                double y = sin * dx + cos * dy + pivot.Y;
+                rotated.Add(new Point((int)Math.Round(x), (int)Math.Round(y)));
+            }
+            return rotated;
+        }
+
+        private void FillImageSet(List<ImageData> set, string prefix)
+        {
+            Directory.Delete(prefix, true);
+            Directory.CreateDirectory(prefix);
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                var files = Directory.GetFiles(folderBrowserDialog1.SelectedPath, "*.dcm");
+                foreach (var file in files)
+                {
+                    var image = new DicomImage(file).RenderImage().AsBitmap();
+                    string newName = prefix +"/"+ Path.GetFileName(file).Replace(".dcm", ".jpg");
+                    image.Save(newName);
+                    image = (Bitmap)Image.FromFile(newName);
+                    ImageData id = new ImageData(image);
+                    set.Add(id);
+                }
+            }
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            label1.Text = (trackBar1.Value * 100 / trackBar1.Maximum) + @"%";
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Directory.Delete("result", true);
+            Directory.CreateDirectory("result");
+            listView1.Items.Clear();
+            imageList2.Images.Clear();
+            int sum = trackBar1.Value;
+            int diffX=0, diffY=0;
+            for (int i=0; i<ImageSetBefore.Count;i++)
+            {
+                var image1 = ImageSetBefore.ElementAt(i);
+                image1.LockBits();
+                var p1 = image1.GetBones(sum);
+
+                var image2 = ImageSetAfter.ElementAt(i);
+                image2.LockBits();
+                var p2 = image2.GetBones(sum);
+
+                //diffX = p2.X - p1.X;
+                //diffY = p2.Y - p1.Y;
+
+                var image = ImageData.Subtract(image1, image2, sum, diffX, diffY);
+                image.Save("result/"+i+".jpg");
+                ImageSetResult.Add(new ImageData((Bitmap) image));
+                imageList2.Images.Add(image);
+                ListViewItem item = new ListViewItem { ImageIndex =i, Text = i.ToString() };
+                listView1.Items.Add(item);
+
+                image1.UnlockBits();
+                image2.UnlockBits();
+
+            }
+            listView1.Update();
+            /*diffX = diffX / ImageSetBefore.Count;
+            diffY = diffY / ImageSetBefore.Count;
+            dataGridView1.Rows.Add(diffX, diffY);*/
+        }
+
+        public void UpdateResult()
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            UpdateResult();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(listView1.SelectedItems.Count>0)
+                pictureBox1.Image = ImageSetResult[listView1.SelectedItems[0].ImageIndex].Source;
+        }
+    }
+}
---
+++ CTScanDyn/ImageData.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTScanDyn
+{
+    class ImageData : IDisposable
+    {
+        public Bitmap Source = null;
+        private BitmapData bData = null;
+
+        public byte[] Pixels { get; set; }
+        public int Height { get; set; }
+        public int Width { get; set; }
+        public int Depth { get; set; }
+        private List<Point> SignificantPoints { get; }
+        public Point CenterOfMass { get; set; }
+        public Point Offset { get; set; }
+        public int RotationDgr { get; set; }
+        public bool IsDisposed { get; set; }
+        private IntPtr Iptr { get; set; }
+        private int rSize { get; set; }
+
+        public ImageData(Bitmap source)
+        {
+            this.Source = source;
+            SignificantPoints = new List<Point>();
+            IsDisposed = false;
+        }
+
+        public Point GetBones(int percent)
+        {
+            for (int y = 0; y < Height; y += 4)
+                for (int x = 0; x < Width; x += 4)
+                {
+
+                    if (Pixels[x * rSize + y] + Pixels[x * rSize + y + 1] + Pixels[x * rSize + y + 2] >= percent)
+                    {
+                        SignificantPoints.Add(new Point(x, y));
+                    }
+                }
+            //CalculateCenter();
+            return CenterOfMass;
+        }
+
+        private void CalculateCenter()
+        {
+            float x = 0f;
+            float y = 0f;
+            foreach (Point p in SignificantPoints)
+            {
+                x += p.X;
+                y += p.Y;
+            }
+            x = x / SignificantPoints.Count;
+            y = y / SignificantPoints.Count;
+            CenterOfMass = new Point((int)x, (int)y);
+        }
+
+        public void LockBits()
+        {
+            try
+            {
+                if (IsDisposed)
+                    throw new ObjectDisposedException(typeof(ImageData).Name);
+
+                Width = Source.Width;
+                Height = Source.Height;
+                Rectangle rect = new Rectangle(0, 0, Width, Height);
+                Depth = Bitmap.GetPixelFormatSize(Source.PixelFormat);
+
+                if (Depth != 8 && Depth != 24 && Depth != 32)
+                {
+                    throw new ArgumentException("Only 8, 24 and 32 bpp images are supported.");
+                }
+
+                bData = Source.LockBits(rect, ImageLockMode.ReadWrite, Source.PixelFormat);
+                rSize = bData.Stride < 0 ? -bData.Stride : bData.Stride;
+                Pixels = new byte[Height * rSize];
+                Iptr = bData.Scan0;
+                for (int y = 0; y < Height; y++)
+                {
+                    Marshal.Copy(IntPtr.Add(Iptr, y * bData.Stride), Pixels, y * rSize, rSize);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        public void UnlockBits()
+        {
+            try
+            {
+                if (IsDisposed)
+                    throw new ObjectDisposedException(typeof(ImageData).Name);
+                if (bData == null)
+                    throw new InvalidOperationException("Image is not locked.");
+
+                // Copy data from byte array to pointer
+                //Marshal.Copy(Pixels, 0, Iptr, Pixels.Length);
+                for (int y = 0; y < Height; y++)
+                {
+                    Marshal.Copy(Pixels, y * rSize, IntPtr.Add(Iptr, y * bData.Stride), rSize);
+                }
+
+                // Unlock bitmap data
+                Source.UnlockBits(bData);
+                bData = null;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public static Image Subtract(ImageData image1, ImageData image2, int percent, int diffX, int diffY)
+        {
+            Bitmap res = new Bitmap(image1.Width, image1.Height);
+            for (int y = 0; y < image1.Height; y++)
+            {
+                for (int x = 0; x < image1.Width; x++)
+                {
+                    if (x + diffX < image1.Width && y - diffY < image1.Height && y - diffY >= 0 && x + diffX >= 0)
+                    {
+                        Color c1 = image1.GetPixel(x + diffX, y - diffY);
+                        Color c2 = image2.GetPixel(x, y);
+                        if (c1.R + c1.G + c1.B >= percent || c2.R + c2.G + c2.B >= percent)
+                        {
+                            res.SetPixel(x, y, SubtractPixel(image2.GetPixel(x, y), image1.GetPixel(x + diffX, y - diffY)));
+                        }
+                        else
+                        {
+                            res.SetPixel(x, y, Color.Black);
+                        }
+                    }
+                }
+            }
+            return res;
+        }
+
+        private static Color SubtractPixel(Color c1, Color c2)
+        {
+            //Color c3 = Color.FromArgb(Math.Abs(c2.R - c1.R), Math.Abs(c2.G - c1.G), Math.Abs(c2.B - c1.B));
+            if (c1.R - c2.R < 0 || c1.G - c2.G < 0 || c1.B - c2.B < 0)
+                return Color.Red;//пропало
+            return Color.Green;//добавилось
+            //return c3;
+        }
+
+        public Color GetPixel(int x, int y)
+        {
+            if (IsDisposed)
+                throw new ObjectDisposedException(typeof(ImageData).Name);
+
+            Color clr = Color.Empty;
+
+            // Get color components count
+            int cCount = Depth / 8;
+
+            // Get start index of the specified pixel
+            int i = (y * rSize) + (x * cCount);
+
+            if (i > Pixels.Length - cCount)
+                throw new IndexOutOfRangeException();
+
+            if (Depth == 32) // For 32 bpp get Red, Green, Blue and Alpha
+            {
+                byte b = Pixels[i];
+                byte g = Pixels[i + 1];
+                byte r = Pixels[i + 2];
+                byte a = Pixels[i + 3]; // a
+                clr = Color.FromArgb(a, r, g, b);
+            }
+            if (Depth == 24) // For 24 bpp get Red, Green and Blue
+            {
+                byte b = Pixels[i];
+                byte g = Pixels[i + 1];
+                byte r = Pixels[i + 2];
+                clr = Color.FromArgb(r, g, b);
+            }
+            if (Depth == 8)
+            // For 8 bpp get color value (Red, Green and Blue values are the same)
+            {
+                byte c = Pixels[i];
+                clr = Color.FromArgb(c, c, c);
+            }
+            return clr;
+        }
+
+        public void SetPixel(int x, int y, Color color)
+        {
+            if (IsDisposed)
+                throw new ObjectDisposedException(typeof(ImageData).Name);
+
+            // Get color components count
+            int cCount = Depth / 8;
+
+            // Get start index of the specified pixel
+            int i = (y * rSize) + (x * cCount);
+            if (i > Pixels.Length - cCount)
+                throw new IndexOutOfRangeException();
+
+            if (Depth == 32) // For 32 bpp set Red, Green, Blue and Alpha
+            {
+                Pixels[i] = color.B;
+                Pixels[i + 1] = color.G;
+                Pixels[i + 2] = color.R;
+                Pixels[i + 3] = color.A;
+            }
+            if (Depth == 24) // For 24 bpp set Red, Green and Blue
+            {
+                Pixels[i] = color.B;
+                Pixels[i + 1] = color.G;
+                Pixels[i + 2] = color.R;
+            }
+            if (Depth == 8)
+            // For 8 bpp set color value (Red, Green and Blue values are the same)
+            {
+                Pixels[i] = color.B;
+            }
+        }
+
+        #region IDisposable Members
+
+        ~ImageData()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (bData != null)
+            {
+                Source.UnlockBits(bData);
+                bData = null;
+            }
+            Source = null;
+            IsDisposed = true;
+        }
+
+        #endregion
+    }
+}
